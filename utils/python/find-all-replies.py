a8867078189a97e20caf546eb8ab2eae6e038d6b
Vladimir Skok <skok@vova.io>
2020-09-06 15:02:10 -0400

    feat: add python utils for local testing

---
+++ utils/python/find-all-replies.py
@@ -0,0 +1,23 @@
+import sys
+import json
+
+f = open('myDatabase.json', 'rt')
+data = json.loads(f.read())
+f.close()
+tweet = data["__collections__"]["posts"]["amstelvar"]["__collections__"]["tweets"]["1302659072550940673"]
+
+# Usage example
+#
+# Find second 4th level reply
+# `python3 find-all-replies.py 0 0 0 1`
+def f(args):
+    r = tweet
+    amount = len(r["replies"])
+    for num in args:
+        r = r["replies"][int(num)]
+        amount = len(r["replies"])
+    return r["text"], amount
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    print(f(args))
