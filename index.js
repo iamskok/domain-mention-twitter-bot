74da0300cd56fcd78012d4e22a67eb941d787b7c
Vladimir Skok <skok@vova.io>
2020-09-08 20:00:29 -0400

    feat: fetch quotes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import timeline from './api/bot/timeline.js';
 import search from './api/bot/search.js';
 import replies from './api/bot/replies.js';
+import quotes from './api/bot/quotes.js';
 import stream from './api/bot/stream.js';
 
 const domainName = process.env.DOMAIN_NAME;
@@ -8,4 +9,5 @@ const domainName = process.env.DOMAIN_NAME;
 timeline(domainName);
 search(domainName);
 replies(domainName);
+quotes(domainName);
 stream(domainName);
