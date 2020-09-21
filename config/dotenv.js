5a8c7d73a02ff2905a4f095bc4458611429450dd
Vladimir Skok <skok@vova.io>
2020-09-21 19:49:31 -0400

    feat: add logging for dotenv config

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
+import logger from '../services/logger.js';
 import envPath from '../constants/env-path.js';
 
-const dotenvConfig = dotenv.config({ path: envPath });
+if (fs.existsSync(envPath)) {
+  logger.log('info', '`.env` was found.');
+} else {
+  logger.log('error', '`.env` is not found.');
+}
 
-export default dotenvConfig;
+export default dotenv.config({ path: envPath });
