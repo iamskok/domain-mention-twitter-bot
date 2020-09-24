7e72f6a275e4b4586bec0ea5e3ab893d2e8c514b
Vladimir Skok <skok@vova.io>
2020-09-24 18:47:59 -0400

    feat: write verbose and exception logs

--- services/logger.js
+++ services/logger.js
@@ -5,14 +5,16 @@ const levels = {
   error: 0,
   warn: 1,
   info: 2,
-  debug: 3,
+  verbose: 3,
+  debug: 4,
 };
 
 const colors = {
   error: 'underline red',
   warn: 'yellow',
   info: 'cyan',
-  debug: 'green',
+  verbose: 'green',
+  debug: 'white',
 };
 
 winston.addColors(colors);
@@ -22,7 +24,7 @@ const { createLogger, format, transports } = winston;
 const logger = createLogger({
   levels,
   level: 'debug',
-  // Don't exit after logging an uncaughtException.
+  // Don't exit after logging an `uncaughtException`.
   exitOnError: false,
   format: format.combine(
     format.timestamp({
@@ -35,25 +37,35 @@ const logger = createLogger({
   transports: [
     // Write error logs to `/logs/error.log`.
     new transports.File({
-      filename: `${path.resolve()}/logs/error.log`, level: 'error',
+      filename: `${path.resolve()}/logs/error.log`,
+      level: 'error',
     }),
     // Write error, warning and info logs to `/logs/info.log`.
     new transports.File({
       filename: `${path.resolve()}/logs/info.log`,
       level: 'info',
-      // handleRejections: true,
+    }),
+    // Write all error, warning, info and verbose logs to `/logs/verbose.log`.
+    new transports.File({
+      filename: `${path.resolve()}/logs/verbose.log`,
+      level: 'verbose',
     }),
     // Write all logs to `/logs/combined.log`.
     new transports.File({
       filename: `${path.resolve()}/logs/combined.log`,
       level: 'debug',
-      // handleRejections: true,
+    }),
+  ],
+  exceptionHandlers: [
+    new transports.File({
+      filename: `${path.resolve()}/logs/exceptions.log`,
     }),
   ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new transports.Console({
+    level: 'verbose',
     format: format.combine(
       format.colorize(),
       format.simple(),
