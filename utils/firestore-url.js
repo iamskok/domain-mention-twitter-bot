bcf2155e7dd6854056523bb17d0d93425401057c
Vladimir Skok <skok@vova.io>
2020-09-26 18:10:22 -0400

    fix: add default params for firestoreURL util

--- utils/firestore-url.js
+++ utils/firestore-url.js
@@ -4,7 +4,7 @@ const firestoreURL = ({
   postTitle,
   tweetId,
   tweet,
-}) => {
+} = {}) => {
   // Tweet document URL.
   if (postTitle && tweetId) {
     return `https://console.firebase.google.com/project/${FIREBASE_PROJECT_ID}/firestore/data~2Fposts~2F${postTitle}~2Ftweets~2F${tweetId}`;
@@ -23,13 +23,5 @@ const firestoreURL = ({
   // Posts collection URL.
   return `https://console.firebase.google.com/project/${FIREBASE_PROJECT_ID}/firestore/data~2Fposts`;
 };
-
-// const firestoreURL = (postTitle, tweetId) => {
-//     if (tweetId) {
-//       return `https://console.firebase.google.com/project/${FIREBASE_PROJECT_ID}/firestore/data~2Fposts~2F${postTitle}~2Ftweets~2F${tweetId}`;
-//     }
-
-//     return `https://console.firebase.google.com/project/${FIREBASE_PROJECT_ID}/firestore/data~2Fposts~2F${postTitle}`;
-//   };
 
 export default firestoreURL;
