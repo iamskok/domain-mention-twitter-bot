f42be09434500ffdbeb012c9530466aa4c318eaa
Vladimir Skok <skok@vova.io>
2020-09-26 16:23:11 -0400

    feat: add db logging

--- api/firestore/db.js
+++ api/firestore/db.js
@@ -1,5 +1,18 @@
 import firebase from '../../services/firebase.js';
+import logger from '../../services/logger.js';
 
-const db = firebase.firestore();
+const db = [];
 
-export default db;
+try {
+  db[0] = firebase.firestore();
+
+  logger.log('info', 'Initialise `firestore`');
+} catch (error) {
+  logger.log('error', '`db`', {
+    errorObject: error,
+  });
+
+  throw new Error(error);
+}
+
+export default db[0];
