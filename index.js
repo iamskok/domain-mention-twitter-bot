a099bc6d6ce06abc209bf046ac2a15c27cf5683c
Vladimir Skok <skok@vova.io>
2020-10-01 22:29:57 -0400

    refactor: rename api/bot => api/core

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import timeline from './src/api/bot/timeline';
-import search from './src/api/bot/search';
-import responses from './src/api/bot/responses';
-import stream from './src/api/bot/stream';
-import comments from './src/api/bot/comments';
+import timeline from './src/api/core/timeline';
+import search from './src/api/core/search';
+import responses from './src/api/core/responses';
+import stream from './src/api/core/stream';
+import comments from './src/api/core/comments';
 
 const { DOMAIN_NAME } = process.env;
 
