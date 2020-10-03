f1134da6639ba60a630206c26a3d5cca7cb6dd8d
Vladimir Skok <skok@vova.io>
2020-10-02 23:17:30 -0400

    chore: add comments for timestamp utils

--- src/utils/timestamp.js
+++ src/utils/timestamp.js
@@ -1,7 +1,9 @@
 import moment from 'moment';
 
+// Format '2020-10-03T00:00:00.000Z` timestamp.
 const loggerTimestamp = (time) => moment(time).format('YYYY-MM-DD HH:mm:ss:SSS');
 
+// Format twitter `Sun Oct 03 00:00:00 +0000 2020` timestamp.
 const commentTimestamp = (time) => moment(Date.parse(time)).format('MM-DD-YYYY');
 
 export { loggerTimestamp, commentTimestamp };
