b083247b745f0ed666105027a97af62ec5fe611a
Vladimir Skok <skok@vova.io>
2020-10-02 22:58:57 -0400

    fix: twitter timestamp formatting

--- src/utils/__tests__/timestamp.spec.js
+++ src/utils/__tests__/timestamp.spec.js
@@ -2,14 +2,14 @@ import { loggerTimestamp, commentTimestamp } from '../timestamp';
 
 describe('loggerTimestamp utility', () => {
   it('returns formatted `YYYY-MM-DD HH:mm:ss:SSS` string', () => {
-    expect(loggerTimestamp('1601687314622'))
-      .toBe('2020-10-02 21:09:52:801');
+    expect(loggerTimestamp('2020-10-03T00:00:00.000Z'))
+      .toBe('2020-10-03 00:00:00:000');
   });
 });
 
 describe('commentTimestamp utility', () => {
   it('returns formatted `MM-DD-YYYY` string', () => {
-    expect(commentTimestamp('Wed Sep 30 17:52:59 +0000 2020'))
-      .toBe('10-02-2020');
+    expect(commentTimestamp('Sun Oct 03 00:00:00 +0000 2020'))
+      .toBe('10-03-2020');
   });
 });
--- src/utils/timestamp.js
+++ src/utils/timestamp.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
-const loggerTimestamp = () => moment().format('YYYY-MM-DD HH:mm:ss:SSS');
+const loggerTimestamp = (time) => moment(time).format('YYYY-MM-DD HH:mm:ss:SSS');
 
-const commentTimestamp = () => moment().format('MM-DD-YYYY');
+const commentTimestamp = (time) => moment(Date.parse(time)).format('MM-DD-YYYY');
 
 export { loggerTimestamp, commentTimestamp };
