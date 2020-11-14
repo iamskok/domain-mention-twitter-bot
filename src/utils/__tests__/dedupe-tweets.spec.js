3c0aa0418b1d66110fefb7835c4d6ebf0ffa94a5
Vladimir Skok <skok@vova.io>
2020-11-14 11:58:13 -0500

    test: add duplicateTweets mock

--- src/utils/__tests__/dedupe-tweets.spec.js
+++ src/utils/__tests__/dedupe-tweets.spec.js
@@ -1,40 +1,37 @@
 import dedupeTweets from '../dedupe-tweets';
+import duplacateTweetsMock from '../../mocks/duplicateTweets';
 
 describe('dedupeTweets utility', () => {
-  const oldTweets = [
-    { id_str: '1113510525045624835' },
-    { id_str: '2213510525045624835' },
-    { id_str: '3313510525045624835' },
-  ];
-
-  const newTweets = [
-    { id_str: '4413510525045624835' },
-    { id_str: '5513510525045624835' },
-    { id_str: '6613510525045624835' },
-  ];
-
   it('returns array of unique tweets when new tweets have copies of the old tweets', () => {
     expect(dedupeTweets(
-      oldTweets,
+      duplacateTweetsMock.oldTweets,
       [
-        ...newTweets,
-        { id_str: '1113510525045624835' },
-        { id_str: '2213510525045624835' },
-        { id_str: '3313510525045624835' },
+        ...duplacateTweetsMock.oldTweets,
+        ...duplacateTweetsMock.newTweets,
       ],
+    )).toMatchSnapshot();
+  });
+
+  it('returns array of unique tweets when old tweets have copies of the new tweets', () => {
+    expect(dedupeTweets(
+      [
+        ...duplacateTweetsMock.oldTweets,
+        ...duplacateTweetsMock.newTweets,
+      ],
+      duplacateTweetsMock.newTweets,
     )).toMatchSnapshot();
   });
 
   it('returns array of new tweets when there are no old tweets', () => {
     expect(dedupeTweets(
       [],
-      newTweets,
+      duplacateTweetsMock.newTweets,
     )).toMatchSnapshot();
   });
 
   it('returns array of old tweets when there are no new tweets', () => {
     expect(dedupeTweets(
-      oldTweets,
+      duplacateTweetsMock.oldTweets,
       [],
     )).toMatchSnapshot();
   });
