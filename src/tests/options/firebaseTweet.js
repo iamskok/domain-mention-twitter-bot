9311e276c2c2b01278f5c073b0e888a47d67e10a
Vladimir Skok <skok@vova.io>
2020-11-15 13:08:20 -0500

    test: add sort-tweets-by-time test suite

---
+++ src/tests/options/firebaseTweet.js
@@ -0,0 +1,151 @@
+const firebaseTweet = {
+  created_at: 'Tue Oct 02 01:00:00 +0000 2012',
+  quotes: [
+    {
+      created_at: 'Wed Oct 03 02:00:00 +0000 2012',
+      quotes: [
+        {
+          created_at: 'Wed Oct 03 03:00:00 +0000 2012',
+          quotes: [
+            {
+              created_at: 'Thu Oct 04 04:00:00 +0000 2012',
+              quotes: [
+                { created_at: 'Thu Oct 04 07:00:00 +0000 2012' },
+                { created_at: 'Thu Oct 04 06:00:00 +0000 2012' },
+                { created_at: 'Thu Oct 04 05:00:00 +0000 2012' },
+              ],
+              replies: [
+                { created_at: 'Thu Oct 04 07:00:00 +0000 2012' },
+                { created_at: 'Thu Oct 04 08:00:00 +0000 2012' },
+                { created_at: 'Thu Oct 04 05:00:00 +0000 2012' },
+              ],
+            },
+          ],
+          replies: [
+            {
+              created_at: 'Fri Oct 05 01:00:00 +0000 2012',
+              quotes: [
+                { created_at: 'Fri Oct 05 04:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 02:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 03:00:00 +0000 2012' },
+              ],
+              replies: [
+                { created_at: 'Fri Oct 05 01:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 02:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 03:00:00 +0000 2012' },
+              ],
+            },
+          ],
+        },
+        { created_at: 'Fri Oct 05 01:00:00 +0000 2012' },
+        { created_at: 'Fri Oct 05 02:00:00 +0000 2012' },
+      ],
+      replies: [
+        {
+          created_at: 'Wed Oct 03 01:00:00 +0000 2012',
+          quotes: [
+            {
+              created_at: 'Thu Oct 04 02:00:00 +0000 2012',
+              quotes: [
+                { created_at: 'Fri Oct 05 05:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 03:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 04:00:00 +0000 2012' },
+              ],
+              replies: [
+                { created_at: 'Fri Oct 05 05:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 03:00:00 +0000 2012' },
+                { created_at: 'Fri Oct 05 04:00:00 +0000 2012' },
+              ],
+            },
+          ],
+          replies: [
+            {
+              created_at: 'Wed Oct 03 05:00:00 +0000 2012',
+              quotes: [
+                { created_at: 'Tue Oct 02 08:00:00 +0000 2012' },
+                { created_at: 'Tue Oct 02 07:00:00 +0000 2012' },
+                { created_at: 'Tue Oct 02 06:00:00 +0000 2012' },
+              ],
+              replies: [
+                { created_at: 'Thu Oct 04 06:00:00 +0000 2012' },
+                { created_at: 'Thu Oct 04 08:00:00 +0000 2012' },
+                { created_at: 'Thu Oct 04 07:00:00 +0000 2012' },
+              ],
+            },
+          ],
+        },
+        { created_at: 'Wed Oct 03 09:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 10:00:00 +0000 2012' },
+      ],
+    },
+    {
+      created_at: 'Wed Oct 03 10:00:00 +0000 2012',
+      quotes: [
+        { created_at: 'Wed Oct 03 11:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 13:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 12:00:00 +0000 2012' },
+      ],
+      replies: [
+        { created_at: 'Wed Oct 03 11:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 15:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 12:00:00 +0000 2012' },
+      ],
+    },
+    {
+      created_at: 'Thu Oct 04 15:00:00 +0000 2012',
+      quotes: [
+        { created_at: 'Thu Oct 04 18:00:00 +0000 2012' },
+        { created_at: 'Thu Oct 04 16:00:00 +0000 2012' },
+        { created_at: 'Thu Oct 04 17:00:00 +0000 2012' },
+      ],
+      replies: [
+        { created_at: 'Thu Oct 04 18:00:00 +0000 2012' },
+        { created_at: 'Thu Oct 04 16:00:00 +0000 2012' },
+        { created_at: 'Thu Oct 04 17:00:00 +0000 2012' },
+      ],
+    },
+  ],
+  replies: [
+    {
+      created_at: 'Tue Oct 02 15:00:00 +0000 2012',
+      quotes: [
+        { created_at: 'Tue Oct 02 19:00:00 +0000 2012' },
+        { created_at: 'Tue Oct 02 17:00:00 +0000 2012' },
+        { created_at: 'Tue Oct 02 18:00:00 +0000 2012' },
+      ],
+      replies: [
+        { created_at: 'Tue Oct 02 19:00:00 +0000 2012' },
+        { created_at: 'Tue Oct 02 16:00:00 +0000 2012' },
+        { created_at: 'Tue Oct 02 17:00:00 +0000 2012' },
+      ],
+    },
+    {
+      created_at: 'Wed Oct 03 19:00:00 +0000 2012',
+      quotes: [
+        { created_at: 'Wed Oct 03 23:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 21:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 22:00:00 +0000 2012' },
+      ],
+      replies: [
+        { created_at: 'Wed Oct 03 23:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 21:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 22:00:00 +0000 2012' },
+      ],
+    },
+    {
+      created_at: 'Wed Oct 03 20:00:00 +0000 2012',
+      quotes: [
+        { created_at: 'Wed Oct 03 23:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 21:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 22:00:00 +0000 2012' },
+      ],
+      replies: [
+        { created_at: 'Wed Oct 03 23:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 22:00:00 +0000 2012' },
+        { created_at: 'Wed Oct 03 21:00:00 +0000 2012' },
+      ],
+    },
+  ],
+};
+
+export default firebaseTweet;
