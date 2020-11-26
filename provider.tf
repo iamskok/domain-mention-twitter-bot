957c914e8eafe8bf4fed8099dd2afd419662eec2
Vladimir Skok <skok@vova.io>
2020-11-26 18:48:12 -0500

    chore: edit digital ocean droplet name

--- provider.tf
+++ provider.tf
@@ -20,9 +20,9 @@ data "digitalocean_ssh_key" "terraform" {
   name = var.do_ssh_key_name
 }
 
-resource "digitalocean_droplet" "production" {
+resource "digitalocean_droplet" "docker-host" {
   image              = "ubuntu-20-04-x64"
-  name               = "production"
+  name               = "docker-host"
   region             = "sfo3"
   size               = "s-1vcpu-1gb"
   private_networking = true
